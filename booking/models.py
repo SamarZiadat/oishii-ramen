6b98dec472fd45608e77caa668af2dd05f5c757d
SamarZiadat <samarziadat@gmail.com>
2023-05-12 11:25:20 +0000

    build: import cloudinary to models

--- booking/models.py
+++ booking/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 SKILL_LEVEL = ((0, "Beginner"), (1, "Intermediate"), (2, "Advanced"))
 
