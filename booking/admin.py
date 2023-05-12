b43c9423a50c597d38e5aab7a9ccb703cb627303
SamarZiadat <samarziadat@gmail.com>
2023-05-12 14:11:48 +0000

    build: add list filter and prepopulated field to course admin

--- booking/admin.py
+++ booking/admin.py
@@ -6,4 +6,6 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Course)
 class CourseAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('skill_level', 'location')
     summernote_fields = ('content')
