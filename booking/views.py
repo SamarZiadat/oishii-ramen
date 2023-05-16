49d166a70c8b5068816e1fe640cc44cc12dc6e61
SamarZiadat <samarziadat@gmail.com>
2023-05-16 12:34:03 +0000

    build: create list view for course summaries

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from models import Course
 
-# Create your views here.
+
+class CourseList(generic.ListView):
+    """
+    Retrieve course summaries and set pagination
+    """
+    model = Course
+    queryset = Course.objects.filter(status=1).order_by('skill_level')
+    template_name = 'index.html'
+    paginate_by = 6
