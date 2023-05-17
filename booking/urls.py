48d75e615512e5c40df14af6440cf397f30bc3f6
SamarZiadat <samarziadat@gmail.com>
2023-05-17 16:11:15 +0000

    build: course detail template and configure urls

--- booking/urls.py
+++ booking/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.CourseList.as_view(), name='home'),
+        path('<slug:slug>/', views.CourseDetail.as_view(), name='course_detail'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Course
 
 
@@ -11,3 +11,19 @@ class CourseList(generic.ListView):
     queryset = Course.objects.filter(status=1).order_by('skill_level')
     template_name = 'index.html'
     paginate_by = 6
+
+class CourseDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Course.objects.filter(status=1)
+        course = get_object_or_404(queryset, slug=slug)
+        reviews = course.reviews.filter(approved=True).order_by("-created_on")
+
+        return render(
+            request,
+            "course_detail.html",
+            {
+                "course": course,
+                "reviews": reviews
+            },
+        )
